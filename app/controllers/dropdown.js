67d9c3fdc9c405fe4b290fc3bb9b9ebe53649509
Veer Jain <veer.jain@globallogic.com>
2012-10-10 17:49:39 +0530

    Resolved dropdown textfield click issue

--- app/controllers/dropdown.js
+++ app/controllers/dropdown.js
@@ -1,18 +1,18 @@
 var isClicked = false;
-$.dropDownBtn.addEventListener('click', function(e){
+$.parentView.addEventListener('click', function(){
 	
 	//Ti.App.fireEvent('showCategory');	
 	Alloy.CFG.categoryTable.visible = false;
-	e.source.backgroundImage = 'arrowDrop.png';
-	e.source.height = 13;
-	e.source.width = 9;
+	$.dropDownBtn.backgroundImage = 'arrowDrop.png';
+	$.dropDownBtn.height = 13;
+	$.dropDownBtn.width = 9;
 	
 	if(!isClicked){
 		isClicked = true;
 		Alloy.CFG.categoryTable.visible = true;
-		e.source.backgroundImage = 'arrowDown.png';
-		e.source.width = 13;
-		e.source.height = 9;
+		$.dropDownBtn.backgroundImage = 'arrowDown.png';
+		$.dropDownBtn.width = 13;
+		$.dropDownBtn.height = 9;
 		
 	}else{
 		isClicked = false;
