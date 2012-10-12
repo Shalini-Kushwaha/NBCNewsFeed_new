519974a00dcf1ec44e078ce9c9d37b18ef9a4b84
Veer Jain <veer.jain@globallogic.com>
2012-10-12 17:41:08 +0530

    Delete favorites functionality added

--- app/controllers/widgetView.js
+++ app/controllers/widgetView.js
@@ -1,5 +1,13 @@
 var params = arguments[0]||{};
 
 $.label.setText(params.titleText);
+$.button.id = params.titleText;
 // 
 // $.tblFavorites
+$.button.addEventListener('click',function(e){
+    var db = Ti.Database.open('nbcNews'); //)
+    db.execute('DELETE FROM favorites where title=?',e.source.id);
+    $.view.visible=false;
+    $.view.height=0;
+    db.close();
+});
