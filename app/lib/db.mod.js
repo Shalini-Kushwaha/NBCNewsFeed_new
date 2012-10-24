b7e379f0bfedf2cd83cdbb976fd0f78a1bdad8d7
Veer Jain <veer.jain@globallogic.com>
2012-10-24 16:21:02 +0530

    db file issue resolved

--- app/lib/db.mod.js
+++ app/lib/db.mod.js
@@ -4,7 +4,7 @@ var db = Ti.Database.open(dbName);
 db.execute('CREATE TABLE IF NOT EXISTS favorites(title TEXT, url TEXT); ');
 db.close();
 
-function selectFromFavorites() {
+exports.selectFromFavorites = function() {
     console.log('Selecting Data from Favorites Table');
     var db = Ti.Database.open(dbName);
     var result = db.execute("SELECT * FROM favorites");
@@ -13,13 +13,13 @@ function selectFromFavorites() {
     return result;
 };
 
-function deleteFromFavorites(profiles, user) {
+exports.deleteFromFavorites = function() {
     console.log('Deleting Data from Favorites Table');
     var db = Ti.Database.open(dbName);
     db.close();
 };
 
-function insertIntoFavorites = function(args) {
+exports.insertIntoFavorites = function() {
     console.log('Inserting Data in Favorites Table');
     var db = Ti.Database.open(dbName);
     db.close();
